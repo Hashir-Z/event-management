f8613230c377fb6b660164010802e88ac099a0c7
Hashir <hashir2k@yahoo.com>
2025-02-05 18:26:27 -0600

    Hashir Bin Zahid: Rearranged a few containers, redesigned to make the layout responsive.

--- event_management_frontend/src/components/Header/Header.tsx
+++ event_management_frontend/src/components/Header/Header.tsx
@@ -5,18 +5,13 @@ import { faUser, faBell } from '@fortawesome/free-solid-svg-icons';
 export function Header() {
   return (
     <div className={styles.Header}>
-      <div className={styles.logoContainer}>
-        <img src="logo-white.svg" alt="logo" className={styles.logoImg} />
+      <div className={styles.logoText}>
+        EventAid
       </div>
-      <div className={styles.headerContainer}>
-        <div className={styles.logoText}>
-          EventAid
-        </div>
-        <div className={styles.iconsContainer}>
-          <FontAwesomeIcon icon={faBell} className={styles.icon} />
-          <FontAwesomeIcon icon={faUser} className={styles.icon} />
-        </div>
-      </div >
+      <div className={styles.iconsContainer}>
+        <FontAwesomeIcon icon={faBell} className={styles.icon} />
+        <FontAwesomeIcon icon={faUser} className={styles.icon} />
+      </div>
     </div >
   );
 }
--- event_management_frontend/src/components/Sidebar/Sidebar.tsx
+++ event_management_frontend/src/components/Sidebar/Sidebar.tsx
@@ -5,15 +5,16 @@ import styles from './Sidebar.module.css';
 export function Sidebar() {
   return (
     <div className={styles.sidebar}>
-      <div className={styles.sidebarContainer}>
+      <div className={styles.logoContainer}>
+        <img src="logo-white.svg" alt="logo" className={styles.logoImg} />
         <hr className={styles.divider} />
-        <div className={styles.menu}>
-          <FontAwesomeIcon icon={faHome} className={styles.icon} />
-        </div>
-        <div className={styles.bottomIcons}>
-          <FontAwesomeIcon icon={faCog} className={styles.icon} />
-          <FontAwesomeIcon icon={faSignOutAlt} className={styles.icon} />
-        </div>
+      </div>
+      <div className={styles.menu}>
+        <FontAwesomeIcon icon={faHome} className={styles.icon} />
+      </div>
+      <div className={styles.bottomIcons}>
+        <FontAwesomeIcon icon={faCog} className={styles.icon} />
+        <FontAwesomeIcon icon={faSignOutAlt} className={styles.icon} />
       </div>
     </div>
   );
