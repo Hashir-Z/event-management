dbfed3d5cd89881fd5f3fa27162f935fd1ece3a9
Pedro Bavdaz <56270205+PedroBavdaz@users.noreply.github.com>
2025-02-14 19:28:14 -0600

    Admin Login
    Added admin Login component

--- event_management_frontend/src/components/index.ts
+++ event_management_frontend/src/components/index.ts
@@ -4,6 +4,6 @@ export {EventForm} from './EventForm';
 export {VolunteerDetails} from './VolunteerDetails';
 export {AdminDashboard} from './AdminDashboard';
 export {VolunteerDashboard} from './VolunteerDashboard';
+export {AdminLogin} from './AdminLogin';
 export {VolunteerHistory} from './VolunteerHistory';
-
 export {VolunteerMatchingForm} from './VolunteerMatchingForm';
